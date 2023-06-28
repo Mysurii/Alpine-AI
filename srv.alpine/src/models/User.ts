f4146bfea944ae8dbd2151fb8c12b05eb1e588e6
Ali Ozcan <ali.ozcan@hva.nl>
2023-06-28 11:24:11 +0200

    refactor: remove unused code

--- srv.alpine/src/models/User.ts
+++ srv.alpine/src/models/User.ts
@@ -1,11 +1,9 @@
 import type { LoginRequest, Tokens, Role } from '../types/account'
 import type { BaseEntity } from './BaseEntity'
-import type { Customization } from './Customization'
 
 export type User = BaseEntity &
   LoginRequest & {
     name: string
     role: Role
     tokens: Tokens
-    customization: Customization
   }
