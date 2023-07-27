b2b95307205d020355150b0ed565144326de838c
Ali Ozcan <ali.ozcan@hva.nl>
2023-07-27 14:20:57 +0200

    feat added error handlers

---
+++ srv.chatbot/app/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request, abort
+from .config.extensions import mongo
+from .config.settings import MONGO_URI, ENVIRONMENT, ALPINE_SERVER
+from .controllers.chatbot import chatbot_bp
+from .utils.exceptions import BaseError
+
+def create_app(db_uri=MONGO_URI):
+  app = Flask(__name__)
+  app.config['MONGO_URI'] = db_uri
+
+  mongo.init_app(app)
+
+  app.register_blueprint(chatbot_bp)
+
+  @app.before_request
+  def allow_alpine_server_requests_only():
+      if ENVIRONMENT == 'production' and request.remote_addr != ALPINE_SERVER:
+          abort(401)
+
+  @app.errorhandler(BaseError)
+  def handle_exception(err):
+    response = {
+      "error": err.error,
+      "message": err.message
+    }
+    if len(err.args) > 0:
+        response["message"] = err.args[0]
+    return jsonify(response), err.status
+
+  return app
