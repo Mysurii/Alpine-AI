ca4bbf117e18c1946d03c700dade7ba001a53bc6
Ali Ozcan <ali.ozcan@hva.nl>
2023-08-20 21:05:48 +0200

    Init spring backend

---
+++ alpine/src/main/java/alpine/alpine/AlpineApplication.java
@@ -0,0 +1,12 @@
+package alpine.alpine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlpineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlpineApplication.class, args);
+    }
+}
