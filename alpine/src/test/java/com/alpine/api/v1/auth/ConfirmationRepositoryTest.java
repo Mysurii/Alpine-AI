cb45c0ccfe767ca822b7204d6fae8df2fb956b81
Ali Ozcan <ali.ozcan@hva.nl>
2023-08-30 13:26:44 +0200

    test: confirmation repo

---
+++ alpine/src/test/java/com/alpine/api/v1/auth/ConfirmationRepositoryTest.java
@@ -0,0 +1,54 @@
+package com.alpine.api.v1.auth;
+
+import com.alpine.api.v1.user.User;
+import com.alpine.api.v1.user.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+class ConfirmationRepositoryTest {
+
+    @Autowired
+    private ConfirmationRepository confirmationRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void itShouldFindConfirmationByCode() {
+        //given
+        User user = new User();
+        user.setName("John Doe");
+        user.setEmail("john@doe.com");
+        user.setPassword("Password");
+
+
+
+        Confirmation confirmation = new Confirmation(user);
+
+        userRepository.save(user);
+        confirmationRepository.save(confirmation);
+
+        //when
+        Optional<Confirmation> foundConfirmation = confirmationRepository.findByToken(confirmation.getToken());
+
+        //then
+        assertThat(foundConfirmation.isPresent()).isTrue();
+    }
+
+    @Test
+    void itShouldNotFindConfirmationCodeWhenItDoesNotExist() {
+        //given
+        String token = "TokenThatDoesNotExist";
+
+        //when
+        Optional<Confirmation> foundConfirmation = confirmationRepository.findByToken(token);
+
+        //then
+        assertThat(foundConfirmation.isPresent()).isFalse();
+    }
+}
